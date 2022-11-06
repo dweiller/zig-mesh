9cef8efbfed62193ad512dffba0984d6e4826b48
dweiller <4678790+dweiller@users.noreply.github.com>
2022-11-06 17:53:42 +1100

    test(bench): add 4 bytes create/destroy benchmarks

--- build.zig
+++ build.zig
@@ -1,15 +1,35 @@
 const std = @import("std");
 
+const bench = @import("src/bench.zig");
+
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const bench = b.addExecutable("bench", "src/bench.zig");
-    bench.setBuildMode(mode);
-
     const bench_step = b.step("bench", "Compile the benchmarks");
-    bench_step.dependOn(&b.addInstallArtifact(bench).step);
+
+    inline for (std.meta.fields(@TypeOf(bench.benchmarks))) |field| {
+        const name = field.name;
+        const step_name = "bench-" ++ name;
+
+        const bench_opts = b.addOptions();
+        bench_opts.addOption([]const u8, "name", name);
+
+        const bench_exe = b.addExecutable(step_name, "src/bench.zig");
+        bench_exe.setBuildMode(mode);
+        bench_exe.addPackage(bench_opts.getPackage("@benchmark"));
+        bench_exe.override_dest_dir = .{ .custom = b.pathJoin(&.{ "bench", @tagName(mode) }) };
+        bench_exe.install();
+
+        const run_step = bench_exe.run();
+        run_step.step.dependOn(&bench_exe.install_step.?.step);
+
+        const step = b.step(step_name, "Run the " ++ name ++ " benchmark");
+        step.dependOn(&run_step.step);
+
+        bench_step.dependOn(&run_step.step);
+    }
 
     const lib_tests = b.addTest("src/test.zig");
     lib_tests.setBuildMode(mode);
--- src/bench.zig
+++ src/bench.zig
@@ -1,22 +1,115 @@
 const std = @import("std");
-const pool_allocator = @import("pool_allocator.zig");
-const PoolAllocator = pool_allocator.PoolAllocator;
+const Allocator = std.mem.Allocator;
+const MeshAllocator = @import("mesh.zig").MeshAllocator;
+
+const benchmark = @import("@benchmark");
 
 pub fn main() !void {
     var args = std.process.args();
     _ = args.next();
-    var count: usize = 100;
+    var count: ?usize = null;
     if (args.next()) |arg| {
         count = try std.fmt.parseInt(usize, arg, 10);
     }
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
-    const Pool = PoolAllocator(16);
-    var pool = try Pool.init(arena.allocator(), 0, 64);
-    defer pool.deinit();
-
-    pool_allocator.benchmarkPoolAllocatorAllocSlot(&pool, count);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var mesher = try MeshAllocator(.{}).init(gpa.allocator());
+    const allocator = mesher.allocator();
+    try @field(benchmarks, benchmark.name)(allocator);
 }
 
+const KiB = 1024;
+const MiB = 1024 * KiB;
+const GiB = 1024 * MiB;
+pub const benchmarks = .{
+    .@"many-u32-16KiB" = benchmarkSize(@sizeOf(u32), 16 * KiB),
+    .@"many-u32-32KiB" = benchmarkSize(@sizeOf(u32), 32 * KiB),
+    .@"many-u32-64KiB" = benchmarkSize(@sizeOf(u32), 64 * KiB),
+    .@"many-u32-128KiB" = benchmarkSize(@sizeOf(u32), 128 * KiB),
+    .@"many-u32-256KiB" = benchmarkSize(@sizeOf(u32), 256 * KiB),
+    .@"many-u32-512KiB" = benchmarkSize(@sizeOf(u32), 512 * KiB),
+};
+
+pub const Benchmark = fn (Allocator) Allocator.Error!void;
+
+pub fn benchmarkLargeBlock(comptime T: type, comptime max_size: usize) Benchmark {
+    const count = max_size / @sizeOf(T);
+    const s = struct {
+        fn f(allocator: Allocator) Allocator.Error!void {
+            var buf = try allocator.alloc(T, count);
+            allocator.free(buf);
+        }
+    };
+    return s.f;
+}
+
+pub fn benchmarkSize(comptime size: usize, comptime max_size: usize) Benchmark {
+    return benchmarkMultiSize(&[1]usize{size}, max_size);
+}
+
+pub fn benchmarkMultiSize(comptime sizes: []const usize, comptime max_size: usize) Benchmark {
+    const sizes_sum = comptime sizes_sum: {
+        var sum = 0;
+        for (sizes) |size| {
+            sum += size;
+        }
+        break :sizes_sum sum;
+    };
+
+    const iterations = max_size / sizes_sum;
+    const s = struct {
+        fn f(allocator: Allocator) Allocator.Error!void {
+            var pointers: [iterations][sizes.len]*anyopaque = undefined;
+            var i: usize = 0;
+            while (i < iterations) : (i += 1) {
+                inline for (sizes) |size, sz| {
+                    pointers[i][sz] = try allocator.create([size]u8);
+                }
+            }
+            i = 0;
+            while (i < iterations) : (i += 1) {
+                inline for (sizes) |size, sz| {
+                    allocator.destroy(@ptrCast(*[size]u8, pointers[i][sz]));
+                }
+            }
+        }
+    };
+    return s.f;
+}
+
+pub fn benchmarkSizeTransposedDestroy(comptime size: usize, comptime max_size: usize) Benchmark {
+    return benchmarkMultiSizeTransposedDestroy(&[1]usize{size}, max_size);
+}
+
+pub fn benchmarkMultiSizeTransposedDestroy(
+    comptime sizes: []const usize,
+    comptime max_size: usize,
+) !Benchmark {
+    const sizes_sum = comptime sizes_sum: {
+        var sum = 0;
+        for (sizes) |size| {
+            sum += size;
+        }
+        break :sizes_sum sum;
+    };
+
+    const iterations = max_size / sizes_sum;
+    const s = struct {
+        fn f(allocator: Allocator) Allocator.Error!void {
+            var pointers: [iterations][sizes.len]*anyopaque = undefined;
+            var i: usize = 0;
+            while (i < iterations) : (i += 1) {
+                inline for (sizes) |size, sz| {
+                    pointers[i][sz] = try allocator.create([size]u8);
+                }
+            }
+            i = 0;
+            inline for (sizes) |size, sz| {
+                while (i < iterations) : (i += 1) {
+                    allocator.destroy(@ptrCast(*[size]u8, pointers[i][sz]));
+                }
+            }
+        }
+    };
+    return s.f;
+}
