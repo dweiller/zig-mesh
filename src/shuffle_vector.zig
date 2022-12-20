ba9a1285cbe376fb8f30ad9ddeeb51918d37d863
dweiller <4678790+dweiller@users.noreply.github.com>
2022-12-20 13:38:41 +1100

    fix: invalid pointers in StaticShuffleVector
    This change makes StaticShuffleVector copyable, which is required for
    the init() function as result location semantics do not guarantee the
    result location of the new vector (at least not in zig 0.11).

--- src/shuffle_vector.zig
+++ src/shuffle_vector.zig
@@ -2,42 +2,15 @@ const std = @import("std");
 const Random = std.rand.Random;
 
 pub fn StaticShuffleVector(comptime max_size: comptime_int) type {
-    return struct {
-        const Self = @This();
-
-        buffer: [max_size]IndexType,
-        vec: ShuffleVector(IndexType),
-
-        pub const IndexType = std.math.IntFittingRange(0, max_size);
-
-        pub fn init(random: std.rand.Random) Self {
-            var self = Self{
-                .buffer = undefined,
-                .vec = undefined,
-            };
-            self.vec = ShuffleVector(IndexType).init(random, self.buffer[0..max_size]);
-            return self;
-        }
-
-        pub inline fn push(self: *Self, index: IndexType) !void {
-            try self.vec.push(index);
-        }
-
-        pub inline fn pushAssumeCapacity(self: *Self, index: IndexType) void {
-            self.vec.pushAssumeCapacity(index);
-        }
-
-        pub inline fn popOrNull(self: *Self) ?IndexType {
-            return self.vec.popOrNull();
-        }
-
-        pub inline fn pop(self: *Self) IndexType {
-            return self.vec.pop();
-        }
-    };
+    const IndexType = std.math.IntFittingRange(0, max_size);
+    return ShuffleVectorGeneric(IndexType, max_size);
 }
 
 pub fn ShuffleVector(comptime T: type) type {
+    return ShuffleVectorGeneric(T, null);
+}
+
+pub fn ShuffleVectorGeneric(comptime T: type, comptime static: ?comptime_int) type {
     comptime {
         if (@typeInfo(T) != .Int and @typeInfo(T).Int.signedness != .unsigned) {
             @compileError("T must be an unsigned integer, got " ++ @typeName(T));
@@ -46,23 +19,43 @@ pub fn ShuffleVector(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        indices: FixedBuffer(IndexType),
+        indices: Buffer,
         random: Random,
+
+        const Buffer = if (static) |max_size| std.BoundedArray(IndexType, max_size) else FixedBuffer(IndexType);
 
         pub const IndexType = T;
 
-        pub fn init(random: Random, buffer: []IndexType) Self {
-            std.debug.assert(buffer.len - 1 <= std.math.maxInt(IndexType));
-            for (buffer) |*index, i| {
-                index.* = @intCast(IndexType, i);
+        pub usingnamespace if (static) |_|
+            struct {
+                pub fn init(random: Random) Self {
+                    var self = Self{
+                        .indices = Buffer{ .len = static.? },
+                        .random = random,
+                    };
+                    var buf = self.indices.slice();
+                    for (buf) |*index, i| {
+                        index.* = @intCast(IndexType, i);
+                    }
+                    random.shuffle(IndexType, buf);
+                    return self;
+                }
             }
-            random.shuffle(IndexType, buffer);
+        else
+            struct {
+                pub fn init(random: Random, buffer: []IndexType) Self {
+                    std.debug.assert(buffer.len - 1 <= std.math.maxInt(IndexType));
+                    for (buffer) |*index, i| {
+                        index.* = @intCast(IndexType, i);
+                    }
+                    random.shuffle(IndexType, buffer);
 
-            return Self{
-                .indices = FixedBuffer(IndexType).initLen(buffer, buffer.len),
-                .random = random,
+                    return Self{
+                        .indices = FixedBuffer(IndexType).initLen(buffer, buffer.len),
+                        .random = random,
+                    };
+                }
             };
-        }
 
         pub fn push(self: *Self, index: IndexType) !void {
             try self.indices.append(index);
@@ -75,9 +68,9 @@ pub fn ShuffleVector(comptime T: type) type {
         }
 
         pub fn pushSwap(self: *Self) void {
-            const len = self.indices.items.len;
+            const len = self.indices.buffer.len;
             const swap_offset = self.random.uintLessThan(usize, len);
-            std.mem.swap(IndexType, &self.indices.items[swap_offset], &self.indices.items[len - 1]);
+            std.mem.swap(IndexType, &self.indices.buffer[swap_offset], &self.indices.buffer[len - 1]);
         }
 
         pub inline fn popOrNull(self: *Self) ?IndexType {
@@ -94,7 +87,7 @@ pub fn FixedBuffer(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        items: []T,
+        buffer: []T,
         capacity: usize,
 
         pub fn init(buffer: []T) Self {
@@ -103,34 +96,34 @@ pub fn FixedBuffer(comptime T: type) type {
 
         pub fn initLen(buffer: []T, len: usize) Self {
             return Self{
-                .items = buffer[0..len],
+                .buffer = buffer[0..len],
                 .capacity = buffer.len,
             };
         }
 
         pub fn hasUnusedCapacity(self: *Self, additional_count: usize) bool {
-            return self.capacity >= self.items.len + additional_count;
+            return self.capacity >= self.buffer.len + additional_count;
         }
 
-        pub fn append(self: *Self, item: T) error{OutOfMemory}!void {
-            if (!self.hasUnusedCapacity(1)) return error.OutOfMemory;
+        pub fn append(self: *Self, item: T) error{Overflow}!void {
+            if (!self.hasUnusedCapacity(1)) return error.Overflow;
             self.appendAssumeCapacity(item);
         }
 
         pub fn appendAssumeCapacity(self: *Self, item: T) void {
-            std.debug.assert(self.items.len < self.capacity);
-            self.items.len += 1;
-            self.items[self.items.len - 1] = item;
+            std.debug.assert(self.buffer.len < self.capacity);
+            self.buffer.len += 1;
+            self.buffer[self.buffer.len - 1] = item;
         }
 
         pub fn pop(self: *Self) T {
-            const val = self.items[self.items.len - 1];
-            self.items.len -= 1;
+            const val = self.buffer[self.buffer.len - 1];
+            self.buffer.len -= 1;
             return val;
         }
 
         pub fn popOrNull(self: *Self) ?T {
-            if (self.items.len == 0) return null;
+            if (self.buffer.len == 0) return null;
             return self.pop();
         }
     };
